5792c3e67be65f8fadba359e047ba46050f55dc9
Agustingmuzak <agustingmuzak@gmail.com>
2025-11-07 02:17:35 -0300

    added new code from dive into systems

--- C-Coding/DiveIntoSystems/main.c
+++ C-Coding/DiveIntoSystems/main.c
@@ -22,7 +22,7 @@ int main(void) {
 
     printf("\n%d + %d = %d", num1, num2, (num1+num2)); */
 
-    char str[50];
+/*     char str[50];
 
     printf("Enter your name: ");
     scanf("%s", &str);
@@ -35,6 +35,41 @@ int main(void) {
     printf("Enter an int and a float: ");
     scanf("%d%g", &x, &pi);
     printf("An int is: %d\nA float is: %g", x, pi);
+ */
+/* 
+    int num1, num2;
+
+    printf("Enter your first number: ");
+    scanf("%d", &num1);
+
+    printf("Enter your second number: ");
+    scanf("%d", &num2);
+
+    if (num1 > num2) {
+        printf("%d is biggest", num1);
+        num2 = num1;
+    } else if (num2 > num1 || num1 == num2) {
+        printf("%d is biggest or %d and %d are the same number", num2, num1, num2);
+        num1 = num2;
+    } */
+    
+
+    int num1, num2;
+
+    /* printf("Enter a number: ");
+    scanf("%d", &num1);
+
+    while (num1 < num2) {
+        printf("%d\n", num1);
+        num1 = num1 + 1;
+    } */
+
+    printf("Choose a number: ");
+    scanf("%d", &num2);
+
+    for (num1 = 0; num1 < num2; num1++) {
+        printf("%d\n", num1);
+    }
 
 
     return 0;
