adca0a00bed9bebdb0ca518c18ad0ccf9c0e08fd
Agustingmuzak <agustingmuzak@gmail.com>
2025-12-03 02:11:49 -0300

    continued work on flask restful

--- Python/2025-11-26 - PY REST/main.py
+++ Python/2025-11-26 - PY REST/main.py
@@ -1,14 +1,39 @@
-from flask import Flask
-from flask_restful import Api, Resource
+from flask import Flask, request
+from flask_restful import Api, Resource, reqparse
 
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        return {"data": "Hello World"}
+
+names = {"micalela": {"age": 27, "gender": "female", "occupation": "cutie"},
+          }
+
+""" class HelloWorld(Resource):
+    def get(self, name):
+        return names[name]
     
-api.add_resource(HelloWorld, "/helloworld")
+    def post(self):
+        return {"data": "Posted"}
+    
+api.add_resource(HelloWorld, "/helloworld/<string:name>") """
+
+video_put_args = reqparse.RequestParser()
+video_put_args.add_argument("name", type=str, help="Name of the video is missing", required=True)
+video_put_args.add_argument("views", type=int, help="Views are missing", required=True)
+video_put_args.add_argument("likes", type=int, help="Likes are missing", required=True)
+
+videos = {}
+
+class Video(Resource):
+    def get(self, video_id):
+        return videos[video_id]
+    
+    def put(self, video_id):
+        args = video_put_args.parse_args()
+        return {video_id: args}
+    
+api.add_resource(Video, "/video/<int:video_id>")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- Python/2025-11-26 - PY REST/test.py
+++ Python/2025-11-26 - PY REST/test.py
@@ -2,5 +2,11 @@ import requests
 
 BASE= "http://127.0.0.1:5000/"
 
-response = requests.get(BASE + "helloworld")
-print(response.json())
+""" response = requests.get(BASE + "helloworld/micalela")
+print(response.json()) """
+
+""" response2 = requests.post(BASE + "helloworld")
+print(response2.json()) """
+
+response3 = requests.put(BASE + "video/1", {"likes": 10})
+print(response3.json())
