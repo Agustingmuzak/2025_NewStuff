ee08ac8bfbb163493cd3282968ea0bdf9c136359
Agustingmuzak <agustingmuzak@gmail.com>
2025-12-04 11:17:53 -0300

    continued adding files to microblog

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,6 +1,17 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello, world!'
+    user = {'username': 'Agustin'}
+    posts = [
+        {
+            'author': {'username': 'Jose'}, 
+            'body': 'Having a great time in Disneyland!'
+        },
+        {
+            'author' : {'username': 'Joaquin'},
+            'body' : 'Anybody selling a couch?'
+        }]
+    return render_template('index.html', title='Home', user=user, posts=posts)
